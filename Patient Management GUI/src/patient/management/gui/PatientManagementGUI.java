7b7d5f296c246269aeffd314eab482d3dfc21e25
Dawid Grad <dawidgrad.work@gmail.com>
2019-01-05 14:53:59 +0000

    Changed folder structure, added login frame

---
+++ Patient Management GUI/src/patient/management/gui/PatientManagementGUI.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patient.management.gui;
+import patient.management.gui.login.*;
+
+/**
+ *
+ * @author Davio
+ */
+public class PatientManagementGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginScreen login = new LoginScreen();
+        login.setVisible(true);
+    }
+    
+}
